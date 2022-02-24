ada39e7008b0fc1e551182515ea7b2c7a5a00510
AngeOro <angelica-om@outlook.com>
2022-02-23 23:38:02 -0500

    Se configura el ms showtime-service y se agregan anotaciones faltantes

---
+++ gateway-service/src/main/java/co/com/poli/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,14 @@
+package co.com.poli.gatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
